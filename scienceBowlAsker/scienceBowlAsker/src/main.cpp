624613d1b11c6e8430ccca2281a617e62575ab99
Geeoon Chung <geeoon13@gmail.com>
2019-09-23 22:37:10 -0500

    Finished

--- scienceBowlAsker/scienceBowlAsker/src/main.cpp
+++ scienceBowlAsker/scienceBowlAsker/src/main.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 #include <string>
-
-bool findSBAFile(std::string path);
-std::string blank;
-std::string filePath;
-std::ofstream sbaFile;
-
+#include <conio.h>
 int main() {
-	std::cout << "Science Bowl Asker" << std::endl;
-	std::cin >> filePath;
-	std::cin.ignore();
-
-	if (!findSBAFile(filePath)) {
-		std::cout << "Count not find sba file at location!";
-		std::cin.get();
-		return 0;
+	char filePath[9999];
+	char word[50];
+	std::string output;
+	std::ifstream fileStream;
+	int qType;
+	int worth;
+	std::cout << "Science Bowl Asker" << std::endl << "SBA file path: ";
+	std::cin.getline(filePath, 9999);
+	fileStream.open(filePath);
+	if (!fileStream.is_open()) {
+		std::cout << "Invalid Path";
+		exit(EXIT_FAILURE);
 	}
-	
 
+	while (fileStream.good()) {
+		fileStream >> word;
+		output = word;
+		if (output == "SBA-END") {
+			std::cout << "\n\n End of Questioning";
+			break;
+		} else if (output == "shortAnswer") {
+			qType = 1;
+			output = "Short Answer  ";
+		} else if (output == "multipleChoice") {
+			qType = 2;
+			output = "Multiple Choice  ";
+		} else if (output == "TOSS-UP") {
+			worth = 4;
+			output = "\n\n TOSS-UP \n";
+		} else if (output == "BONUS") {
+			worth = 10;
+			output = "\n\n BONUS \n";
+		} else if (output == "ANSWER:") {
+			output = "\n ANSWER: ";
+			_getch();
+		} else if (output == "W)") {
+			output = "\nW) ";
+		} else if (output == "X)") {
+			output = "\nX) ";
+		} else if (output == "Y)") {
+			output = "\nY)";
+		} else if (output == "Z)") {
+			output = "\nZ)";
+		}
+		std::cout << output << " ";
+	}
 
+	fileStream.close();
+	_getch();
 	return 0;
-}
-
-bool findSBAFile(std::string path) {
-	sbaFile.open(path);
-	
 }
